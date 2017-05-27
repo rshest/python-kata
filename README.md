8f290117e116087715f464d9f592bd85b2f50f44
Ruslan Shestopalyuk <rushbits@gmail.com>
2017-05-27 20:16:57 +0200

    chore: Initial skeleton for the 'python kata' files

---
+++ README.md
@@ -0,0 +1,1 @@
+Solutions to different little programming exercises, to practice Python skills.
---
+++ udacity/solutions.py
@@ -0,0 +1,31 @@
+
+
+# QUESTION 1
+def question1(s, t):
+    return  True
+
+
+# QUESTION 2
+def question2(s):
+    return ""
+
+
+# QUESTION 3
+def question3(G):
+    return {}
+
+
+# QUESTION 4
+def question4(T, r, n1, n2):
+    return 0
+
+
+# QUESTION 5
+class Node(object):
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+
+def question5(ll, m):
+    return Node()
