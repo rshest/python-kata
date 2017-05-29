7d9e1199160e882eaa26f852660e110da1630b3a
Ruslan Shestopalyuk <rushbits@gmail.com>
2017-05-29 10:57:42 +0200

    feat: Solution to Question 2

--- udacity/solutions.py
+++ udacity/solutions.py
@@ -5,22 +5,20 @@ import unittest
 # QUESTION 1
 ###########################################################
 def question1(s, t):
-
     char_reg = [0]*255
     
     # init character counts according to t
     for c in t:
         char_reg[ord(c)] += 1
-    
     chars_left = len(t)
+
     anagram_start = 0
 
-    for i in xrange(len(s)):
+    for i, cur_char in enumerate(s):
         if chars_left == 0:
             # early exit
             return True
 
-        cur_char = s[i]
         idx = ord(cur_char)
         if char_reg[idx] > 0:
             # consume character from registry
@@ -54,6 +52,7 @@ class Test1(unittest.TestCase):
         self.assertTrue(question1('pythonic', 'noth'))
         self.assertTrue(question1('dddebit carddd', 'bad credit'))
         self.assertTrue(question1('deddbit carddd', 'bad credit'))
+        self.assertTrue(question1('a'*100000, 'a'*100000))
 
         self.assertFalse(question1('pythonic', 'z'))
         self.assertFalse(question1('', 'abc'))
@@ -66,7 +65,42 @@ class Test1(unittest.TestCase):
 # QUESTION 2
 ###########################################################
 def question2(s):
-    return ""
+    n = len(s)
+    if n <= 1:
+        return s
+
+    def longest_palindrome_at(middle_pos, offs=0):
+        """Returns the pair of furthest positions, (from, to), exclusive"""
+        start = middle_pos
+        end = min(middle_pos + offs, n - 1)
+        while start >= 0 and end < n and s[start] == s[end]:
+            start -= 1
+            end += 1
+        return (start, end)
+    
+    palindromes = (longest_palindrome_at(middle_pos, offs) 
+                    for middle_pos in xrange(len(s)) 
+                    for offs in [0, 1])
+                    
+    start, end = max(palindromes, key=lambda (s, e): e - s)
+    return s[start + 1:end]
+
+
+# QUESTION 2 TESTS
+class Test2(unittest.TestCase):
+    def test(self):
+        self.assertEquals(question2(''), '')
+        self.assertEquals(question2('a'), 'a')
+        self.assertEquals(question2('aa'), 'aa')
+        self.assertEquals(question2('ab'), 'a')
+        self.assertEquals(question2('dabacb'), 'aba')
+        self.assertEquals(question2('tdudacityticadutd'), 'udacityticadu')
+        self.assertEquals(question2('daabbaaccb'), 'aabbaa')
+        self.assertEquals(question2('udacity'), 'u')
+        self.assertEquals(question2('udacity'), 'u')
+        self.assertEquals(question2('uudacity'), 'uu')
+        self.assertEquals(question2('uduacity'), 'udu')
+        self.assertEquals(question2('a'*1000), 'a'*1000)
 
 
 ###########################################################
